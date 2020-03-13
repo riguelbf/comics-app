3eb5a7303b4e19b53af3bdd7c1ec1609cb7befaf
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-03-13 13:30:39 -0300

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './routes';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Global from './assets/styles/global';
 
 function App () {
   return (
     <div className="App">
+      <Global />
       <Header />
       <Routes />
     </div>
