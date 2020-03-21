3884464988a314a53b5b6c0b20be79a8addb7fec
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-03-21 11:32:05 -0300

    fixed eslint compile errors

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import PropType from 'prop-types';
 
 import { MdSearch } from 'react-icons/md';
--- src/store/modules/character/sagas.js
+++ src/store/modules/character/sagas.js
@@ -1,4 +1,4 @@
-import { call, put, all, takeLatest, apply } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { API_KEY, HASH, TIME_STAMP } from '../../../config';
 import request from '../../../services/request';
 
